40e7f998e000e1f5245b3b37531ba9ed399716fd
InfiniteNightmare <742851870@qq.com>
2020-08-03 16:40:07 +0800

    Create badapplle

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # badapple
  
+ ## 用于乐咩的整活
+ 
+[b站视频](https://www.bilibili.com/video/BV1Z54y1v7hX)
--- badapple.py
+++ badapple.py
@@ -5,17 +5,14 @@ from moviepy.editor import *
 height = 384
 width = 512
 
-mie_pic = cv2.imread(r'E:\Python\badapple\mie2016.png')
-videopath = 'E:\\Python\\badapple\\1.flv'
+mie_pic = cv2.imread('mie2016.png')
+videopath = '1.flv'
 
 def fl(gf, t):
     img = gf(t)
     temp = np.array(img)
-    # for i in range(height):
-    #     for j in range(width):
-    #         temp[i, j] = ~img[i, j]
     return cv2.bitwise_and(temp, mie_pic)
 
 clip = VideoFileClip(videopath)
 newclip = clip.fl(fl)
-newclip.write_videofile('badmie_1.mp4')
+newclip.write_videofile('video.mp4')
